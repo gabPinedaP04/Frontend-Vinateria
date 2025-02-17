a3d5e5113cbaa23d0b3662700dad33cdf58f70c1
gabPinedaP04 <gab.pinedap04@gmail.com>
2025-02-17 17:57:31 -0600

    agregar get de inventario

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -4,69 +4,53 @@
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" class="px-6 py-3">Nombre del Vino</th>
-            <th scope="col" class="px-6 py-3">Color</th>
-            <th scope="col" class="px-6 py-3">Categoría</th>
             <th scope="col" class="px-6 py-3">Precio</th>
+            <th scope="col" class="px-6 py-3">Categoría</th>
+            <th scope="col" class="px-6 py-3">description</th>
             <th scope="col" class="px-6 py-3">Acción</th>
           </tr>
         </thead>
         <tbody>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Vino Tinto Reserva
-            </th>
-            <td class="px-6 py-4">Tinto</td>
-            <td class="px-6 py-4">Reserva</td>
-            <td class="px-6 py-4">$1200</td>
+          <tr v-for="producto in productos" :key="producto.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+            
+            <td class="px-6 py-4">{{producto.fullName}}</td>
+            <td class="px-6 py-4">{{producto.price}}</td>
+            <td class="px-6 py-4">{{ producto.category }}</td>
+            <td class="px-6 py-4">{{ producto.description }}</td>
+
+
             <td class="px-6 py-4">
               <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
+              <br>
+              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Eliminar</a>
+
             </td>
           </tr>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Chardonnay Premium
-            </th>
-            <td class="px-6 py-4">Blanco</td>
-            <td class="px-6 py-4">Blanco Seco</td>
-            <td class="px-6 py-4">$850</td>
-            <td class="px-6 py-4">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-            </td>
-          </tr>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Merlot Clásico
-            </th>
-            <td class="px-6 py-4">Tinto</td>
-            <td class="px-6 py-4">Joven</td>
-            <td class="px-6 py-4">$650</td>
-            <td class="px-6 py-4">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-            </td>
-          </tr>
-          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Syrah Intenso
-            </th>
-            <td class="px-6 py-4">Tinto</td>
-            <td class="px-6 py-4">Robusto</td>
-            <td class="px-6 py-4">$980</td>
-            <td class="px-6 py-4">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              Espumante Brut
-            </th>
-            <td class="px-6 py-4">Dorado</td>
-            <td class="px-6 py-4">Espumante</td>
-            <td class="px-6 py-4">$1100</td>
-            <td class="px-6 py-4">
-              <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-            </td>
-          </tr>
+          
+        
+          
         </tbody>
       </table>
     </div>
   </template>
+
+  <script setup>
+  import { ref, onMounted } from "vue";
+  import axios, {isCancel, AxiosError} from 'axios';
+
+  const productos = ref([]);
+
+const obtenerProductos = async () => {
+  try {
+    const respuesta = await axios.get("http://184.73.65.126/product"); // Reemplaza con tu API real
+    console.log("Resúesta :: ", respuesta)
+    productos.value = respuesta.data._embedded.product;
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+  }
+};
+
+// Llamar la API cuando el componente se monte
+onMounted(obtenerProductos);
+
+</script>
